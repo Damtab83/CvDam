4db812b4f8f64f49f5a9a096044499efa14dcb63
Damtab83 <damtab@hotmail.com>
2025-02-12 13:29:45 +0100

    created deploy files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Damtab83.github.io",
+  base: "CvDam",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
